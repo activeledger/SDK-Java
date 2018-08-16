01ff774e824f259600e5960b54f9c04a8917b576
Nauman Tariq <nauman@agilitysciences.com>
2018-08-16 17:03:12 +0100

    Commenting main function

--- src/main/java/org/activeledger/java/sdk/activeledgerjavasdk/ActiveledgerJavaSdkApplication.java
+++ src/main/java/org/activeledger/java/sdk/activeledgerjavasdk/ActiveledgerJavaSdkApplication.java
@@ -1,17 +1,11 @@
 package org.activeledger.java.sdk.activeledgerjavasdk;
 
-import java.io.IOException;
-
-import org.activeledger.java.sdk.key.management.Encryption;
-import org.activeledger.java.sdk.key.management.KeyGen;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-
 
 public class ActiveledgerJavaSdkApplication {
-
+/*
 	public static void main(String[] args) throws IOException, Base64DecodingException {
 
 		AbstractApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
@@ -26,7 +20,7 @@ public class ActiveledgerJavaSdkApplication {
          
         context.close();
 	}
-	
+	*/
 	public static AbstractApplicationContext init()  {
 
 		AbstractApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
